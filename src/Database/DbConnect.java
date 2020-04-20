e48da1ac686d330eedbf20d20e2053a65d713689
mesaikat17 <mesaikat17@gmail.com>
2020-04-20 19:58:21 +0530

    Database Config

--- src/Database/DbConnect.java
+++ src/Database/DbConnect.java
@@ -5,9 +5,9 @@ public class DbConnect {
 
 	static Connection cn=null;
 	static String driver="com.mysql.jdbc.Driver";
-	static String url="jdbc:mysql://localhost:3306/ADJAVA";
-	static String user="saikat";
-	static String pass="saikat";
+	static String url="jdbc:postgressql://"+ "ec2-18-233-137-77.compute-1.amazonaws.com:5432"+"/d6hkc11isjn8kf";
+	static String user="sefxjkhlqzfefc";
+	static String pass="4104533756c7bc2e4082b72b616643f9654f2f850f2597b43f2b1e90bc2f5d42";
 	
 	public static Connection getDb()
 	{
